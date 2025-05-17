9e8ec9e566a47781dae49b09483e3c3afe72e734
Akshayansg24 <akshaayag22@gmail.com>
2025-05-17 22:47:46 +0530

    first1

--- deploy.bat
+++ deploy.bat
@@ -1,24 +1,32 @@
 @echo off
+REM Deploy React build locally
+
 REM Set paths
 set SOURCE_DIR=%~dp0build
 set DEST_DIR=C:\ReactDeploy
 
-REM Ensure build exists
+REM Check if build directory exists
 if not exist "%SOURCE_DIR%" (
-  echo Build directory not found. Please run npm run build first.
-  exit /b 1
+    echo [ERROR] Build directory not found at: %SOURCE_DIR%
+    echo Please run "npm run build" first.
+    exit /b 1
 )
 
-REM Create deploy directory if not exists
+REM Create deploy directory if it doesn't exist
 if not exist "%DEST_DIR%" (
-  mkdir "%DEST_DIR%"
+    echo [INFO] Creating destination directory at: %DEST_DIR%
+    mkdir "%DEST_DIR%"
 )
 
-REM Clear old deployment
-del /Q "%DEST_DIR%\*"
-for /d %%x in ("%DEST_DIR%\*") do rmdir /s /q "%%x"
+REM Clear old deployment files
+echo [INFO] Clearing old files in: %DEST_DIR%
+del /Q "%DEST_DIR%\*" 2>nul
+for /D %%x in ("%DEST_DIR%\*") do (
+    rmdir /S /Q "%%x"
+)
 
 REM Copy new build files
-xcopy /E /I /Y "%SOURCE_DIR%\*" "%DEST_DIR%\"
+echo [INFO] Copying files from %SOURCE_DIR% to %DEST_DIR%
+xcopy /E /I /Y "%SOURCE_DIR%\*" "%DEST_DIR%\" >nul
 
-echo Deployment complete.
+echo [SUCCESS] Deployment complete.
